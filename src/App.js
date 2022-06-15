ffa219466fc811af258b48551a20504c4a15cc12
patiname <patiname@naver.com>
2022-06-15 09:57:07 +0900

    props

--- src/App.js
+++ src/App.js
@@ -18,7 +18,17 @@ const App = () => {
       <FnComponent_2 /> */}
       {/* <ClassComponent /> */}
 
-      <FoodMenu />
+      {/* *props
+      =><컴포넌트 프롭스명="값"/> */}
+      <h3>아침메뉴</h3>
+      <FoodMenu menuName="계란밥" sideMenu="물" />
+
+      <h3>점심메뉴</h3>
+      <FoodMenu menuName="김밥" sideMenu="사이다" />
+
+      <h3>저녁메뉴</h3>
+      <FoodMenu menuName="쌈뽭" sideMenu="쇠주" />
+      {/* 2일차 */}
     </div>
   );
 };
