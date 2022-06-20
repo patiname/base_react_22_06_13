33963e2c765e285e7a4dfb40e0779f8d8daf3d43
patiname <patiname@naver.com>
2022-06-20 13:37:29 +0900

    rotate

--- src/components/6_Event/ChoiceColor.js
+++ src/components/6_Event/ChoiceColor.js
@@ -17,6 +17,8 @@ const Box = styled.div`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   margin-bottom: 20px;
+  transform: rotateZ(${(props) => props.rotate});
+  transition: 0.5s;
 `;
 
 const BtnWrap = styled.div`
@@ -31,6 +33,7 @@ const Button = styled.button`
 
 export const ChoiceColor = () => {
   const [color, setColor] = useState("red");
+  const [rotate, setRotate] = useState("0deg");
 
   const handleRedColor = () => setColor("red");
 
@@ -38,14 +41,17 @@ export const ChoiceColor = () => {
 
   const handleBlueColor = () => setColor("blue");
 
+  const handleRotate = () => setRotate("360deg");
+
   return (
     <Wrap>
       <h3>버튼을 눌러 색상을 바꿔보세요!</h3>
-      <Box bgColor={color}></Box>
+      <Box rotate={rotate} bgColor={color}></Box>
       <BtnWrap>
         <Button onClick={handleRedColor}>빨강</Button>
         <Button onClick={handleGreenColor}>초록</Button>
         <Button onClick={handleBlueColor}>파랑</Button>
+        <Button onClick={handleRotate}>회전!</Button>
       </BtnWrap>
     </Wrap>
   );
