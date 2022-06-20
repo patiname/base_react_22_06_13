7420cbb239e99c619581ef2e51165232691bc9e5
patiname <patiname@naver.com>
2022-06-20 10:26:49 +0900

    cover style

--- src/components/5_MusicApp/MusicApp.js
+++ src/components/5_MusicApp/MusicApp.js
@@ -30,9 +30,18 @@ const Section = styled.section`
   background-color: #1d1d1d;
   border-radius: 25px;
   padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const CoverImg = styled.div``;
+const CoverImg = styled.div`
+  width: 90%;
+  height: 280px;
+  border-radius: 25px;
+  box-shadow: 0 15px 20px 10px rgba(0, 0, 0, 0.3);
+  margin: 30px 0;
+`;
 
 export const MusicApp = () => {
   return (
@@ -40,7 +49,11 @@ export const MusicApp = () => {
       <Section>
         <Header />
 
-        <CoverImg />
+        <CoverImg
+          style={{
+            background: `url(https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c516f191023435.5e270367679f1.png) no-repeat center / cover`,
+          }}
+        />
 
         <TitleWrap />
 
