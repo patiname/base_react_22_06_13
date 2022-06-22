2cc789f4df824f136759d0c7012d0b0b182fae17
patiname <patiname@naver.com>
2022-06-22 10:45:10 +0900

    home

--- src/components/8_BaseLayout/pages/Home.js
+++ src/components/8_BaseLayout/pages/Home.js
@@ -1,3 +1,47 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Banner = styled.section`
+  height: 800px;
+  background-color: lightgray;
+`;
+
+const Section = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 120px 400px;
+`;
+
+const Content = styled.div`
+  width: 45%;
+  height: 500px;
+  background-color: lightgray;
+  box-shadow: 25px 25px 30px rgba(0, 0, 0, 0.3);
+  transition: 0.5s;
+  &:hover {
+    transform: translate(25px, 25px);
+    box-shadow: none;
+  }
+  a {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner></Banner>
+
+      <Section>
+        <Content>
+          <Link to="/sub1"></Link>
+        </Content>
+        <Content>
+          <Link to="/sub2"></Link>
+        </Content>
+      </Section>
+    </div>
+  );
 };
