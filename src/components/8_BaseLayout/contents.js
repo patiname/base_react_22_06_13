2e98892bc8b4482a7c72275383d9ed824c3dcff3
patiname <patiname@naver.com>
2022-06-24 08:59:02 +0900

    layout end

---
+++ src/components/8_BaseLayout/contents.js
@@ -0,0 +1,17 @@
+export const sub_1 = {
+  id: 0,
+  title: "서브페이지1",
+  desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to ma`,
+};
+
+export const sub_2 = {
+  id: 1,
+  title: "서브페이지2",
+  desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to ma`,
+};
--- src/components/8_BaseLayout/pages/Sub.js
+++ src/components/8_BaseLayout/pages/Sub.js
@@ -29,18 +29,13 @@ const Desc = styled.p`
   line-height: 1.5rem;
 `;
 
-export const Sub1 = () => {
+export const Sub = ({ cons }) => {
   return (
     <Section>
       <Bg />
       <TextWrap>
-        <Title>서브페이지1</Title>
-        <Desc>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to ma
-        </Desc>
+        <Title>{cons.title}</Title>
+        <Desc>{cons.desc}</Desc>
       </TextWrap>
     </Section>
   );
